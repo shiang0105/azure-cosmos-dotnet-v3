e5a4bac83b18ac4dcadb4262c6dca6a9642b874f
shiang0105 <yitzeng@microsoft.com>
2022-08-24 22:35:23 +0800

    MinThreadPoolSize

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureContainerInstances/run.sh
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureContainerInstances/run.sh
@@ -1,2 +1,2 @@
 cd ..
-dotnet run -c Release -e ${ENDPOINT} -k ${KEY} -t ${THROUGHPUT} -n ${DOCUMENTS} --pl ${PARALLELISM} --cleanuponfinish ${CLEANUPFINISH} -w ${WORKLOAD_NAME}
+dotnet run -c Release -e ${ENDPOINT} -k ${KEY} -t ${THROUGHPUT} -n ${DOCUMENTS} --pl ${PARALLELISM} --cleanuponfinish ${CLEANUPFINISH} -w ${WORKLOAD_NAME} --MinThreadPoolSize ${MinThreadPoolSize}

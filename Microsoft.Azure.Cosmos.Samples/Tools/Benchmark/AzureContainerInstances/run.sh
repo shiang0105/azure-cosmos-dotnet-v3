26911d3324208e382e5c7254d4ad9be2f697bbab
shiang0105 <yitzeng@microsoft.com>
2022-08-24 21:57:05 +0800

    WORKLOAD_NAME

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureContainerInstances/run.sh
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureContainerInstances/run.sh
@@ -1,2 +1,2 @@
 cd ..
-dotnet run -c Release -e ${ENDPOINT} -k ${KEY} -t ${THROUGHPUT} -n ${DOCUMENTS} --pl ${PARALLELISM} --cleanuponfinish ${CLEANUPFINISH} -w {WORKLOADTYPE}
+dotnet run -c Release -e ${ENDPOINT} -k ${KEY} -t ${THROUGHPUT} -n ${DOCUMENTS} --pl ${PARALLELISM} --cleanuponfinish ${CLEANUPFINISH} -w ${WORKLOAD_NAME}

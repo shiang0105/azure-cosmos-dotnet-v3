263e69642e8a26e962c8662e3531de18f992848b
John Tzeng <yitzeng@microsoft.com>
2022-08-25 14:40:41 +0800

    5000 tenant

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/v3/InsertV3BenchmarkOperation.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/v3/InsertV3BenchmarkOperation.cs
@@ -7,6 +7,7 @@ namespace CosmosBenchmark
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Text;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
     using Newtonsoft.Json;
@@ -18,9 +19,23 @@ namespace CosmosBenchmark
         private readonly string partitionKeyPath;
         private readonly Dictionary<string, object> sampleJObject;
 
+        private static readonly string[] tenantIds;
+
         private readonly string databaseName;
         private readonly string containerName;
 
+        private static readonly int tenantCount = 5000;
+
+
+        static InsertV3BenchmarkOperation()
+        {
+            tenantIds = new string[tenantCount];
+
+            for (int i = 0; i < tenantIds.Length; i++)
+            {
+                tenantIds[i] = Guid.NewGuid().ToString();
+            }
+        }
         public InsertV3BenchmarkOperation(
             CosmosClient cosmosClient,
             string dbName,
@@ -37,10 +52,28 @@ namespace CosmosBenchmark
             this.sampleJObject = JsonHelper.Deserialize<Dictionary<string, object>>(sampleJson);
         }
 
+        public static int cnt1 = 0;
+
+        public static int cnt = 0;
+
         public async Task<OperationResult> ExecuteOnceAsync()
         {
+            if (cnt1++ < 2)
+            {
+                Console.WriteLine(partitionKeyPath + "----------------------------");
+                foreach (var k in sampleJObject)
+                {
+                    Console.WriteLine(k.Key + ":" + k.Value);
+                }
+                Console.WriteLine(partitionKeyPath + "----------------------------===========================");
+            }
             using (MemoryStream input = JsonHelper.ToStream(this.sampleJObject))
             {
+                if (cnt++ < 2)
+                {
+                    Console.WriteLine(Encoding.ASCII.GetString(input.ToArray()));
+                    input.Position = 0;
+                }
                 ResponseMessage itemResponse = await this.container.CreateItemStreamAsync(
                         input,
                         new PartitionKey(this.sampleJObject[this.partitionKeyPath].ToString()));
@@ -62,9 +95,10 @@ namespace CosmosBenchmark
 
         public Task PrepareAsync()
         {
-            string newPartitionKey = Guid.NewGuid().ToString();
+            Random rnd = new Random();
+            string tenantId = tenantIds[rnd.Next(tenantCount)];
             this.sampleJObject["id"] = Guid.NewGuid().ToString();
-            this.sampleJObject[this.partitionKeyPath] = newPartitionKey;
+            this.sampleJObject[this.partitionKeyPath] = tenantId;
 
             return Task.CompletedTask;
         }
